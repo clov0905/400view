835a088bd2328d9f5ccc0b09d2959a2f8755a6a4
yanzhenqing1 <1489622871@qq.com>
2022-08-08 23:44:41 +0800

    谭永波++ 23-44

--- src/components/goods.vue
+++ src/components/goods.vue
@@ -13,7 +13,7 @@
   <el-row :gutter="20">
     <el-col :span="8">
       <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getGoodsList">
-        <el-button slot="append" icon="el-icon-search" @click="getGoodsList"></el-button>
+        <el-button slot="append" icon="el-icon-search" @click="findGoodsByName()"></el-button>
       </el-input>
     </el-col>
     <el-col :span="4">
@@ -32,14 +32,14 @@
     <el-table-column label="商品名称" width="180" >
       <template slot-scope="scope">
       <!-- 	<i class="el-icon-time"></i> -->
-      	<span style="margin-left: 10px">{{ scope.row.goodsName }}</span>
+      	<span style="margin-left: 10px">{{ scope.row.goodsName}}</span>
       </template>
 
     </el-table-column>
     <el-table-column label="商品价格(元)" width="180">
       <template slot-scope="scope">
       <!-- 	<i class="el-icon-time"></i> -->
-      	<span style="margin-left: 10px">{{ scope.row.goodsPrice }}</span>
+      	<span style="margin-left: 10px">{{ scope.row.goodsPrice}}</span>
       </template>
     </el-table-column>
     <!-- <el-table-column label="商品图片" prop="goodsImg" width="100px"></el-table-column> -->
@@ -59,10 +59,16 @@
 
   <!-- 分页区域 -->
   <el-pagination
-    background
-    layout="prev, pager, next"
-    :total="1000">
-  </el-pagination>
+     background
+     @next-click="nextClick"
+     @prev-click="prevClick"
+      @size-change="handleSizeChange"
+       @current-change="currentChange"
+     :current-page="currentPage"
+     :page-size:="pagesize"
+     layout="prev, pager, next,total"
+     :total="total">
+   </el-pagination>
 
 </el-card>
 </div>
@@ -73,32 +79,76 @@ export default {
 data(){
 return{
   // 查询参数对象
+
   queryInfo:{
     query:'',
-    pagenum:1,
-    pagesize:5,
+    // pagenum:1,
+    // pagesize:5,
+    goodsName: '',
   },
   // 商品列表
   goodslist:[],
   // 总数据条数
-  total:0
+  total:0,
+  pagesize:6,
+  currentPage:1,
+  search: '',
 }
 },
-created(){
-this.getGoodsList();
-},
+// created(){
+// this.getGoodsList();
+// },
 methods:{
-// 根据分页获取对应的商品列表
-async getGoodsList(){
-const {data:res} = await this.$http.get('goods',{params:this.queryInfo});
-if(res.meta.status !== 200){
-  return this.$message.error('获取商品列表失败！');
-}
-this.$message.success('获取商品列表成功!')
-console.log(res.data);
-this.goodslist = res.data.goods;
-this.total = res.data.total;
-},
+  currentChange(val) {
+  	this.currentPage = val;
+    this.findGoodsByName()
+  },
+  handleSizeChange(psize) {
+      this.pageSize = psize;
+      this.findGoodsByName()
+  },
+  prevClick(){
+    this.$data.currentPage=this.$data.currentPage-1
+    this.getGoodsList("",(this.$data.currentPage-1)*this.$data.pagesize,this.$data.pagesize)
+  },
+  nextClick(){
+    this.$data.currentPage=this.$data.currentPage+1
+    this.getGoodsList("",(this.$data.currentPage-1)*this.$data.pagesize,this.$data.pagesize)
+  },
+  findGoodsByName(){
+     this.getGoodsList(this.$data.search,(this.$data.currentPage-1)*this.$data.pagesize,this.$data.pagesize)
+  },
+  getGoodsList(goodsName,startPage,singlePageNum){
+      this.$axios({
+          url: '/goodsMange/findGoodsListByName/',
+          method: 'post',
+          headers: { "Content-Type": "multipart/form-data" },
+          data:{
+              goodsName:goodsName,
+              startPage:startPage,
+              singlePageNum:singlePageNum,
+          }//传值
+        })
+        .then(res=>{
+          console.log('数据接收:',res.data.list);
+          console.log('数据总量:',res.data.num);
+          this.$data.total=res.data.num
+          //赋值table绑定的数组
+          this.$data.goodslist=res.data.list
+          console.log("当前页:",this.$data.currentPage,"页/条:",this.$data.pagesize,"起始:",(this.$data.currentPage-1)*this.$data.pagesize)
+        });
+  },
+// // 根据分页获取对应的商品列表
+// async getGoodsList(){
+// const {data:res} = await this.$http.get('goods',{params:this.queryInfo});
+// if(res.meta.status !== 200){
+//   return this.$message.error('获取商品列表失败！');
+// }
+// this.$message.success('获取商品列表成功!')
+// console.log(res.data);
+// this.goodslist = res.data.goods;
+// this.total = res.data.total;
+// },
 handleSizeChange(newSize){
 this.queryInfo.pagesize = newSize;
 this.getGoodsList();
@@ -141,7 +191,11 @@ tableRowClassName({
 				}
 				return '';
 			},
-}
+},
+ mounted(){
+        console.log("挂载后：数据已挂载到模板中。。。mounted")
+        this.getGoodsList("",(this.$data.currentPage-1)*this.$data.pagesize,this.$data.pagesize)
+      },
 }
 </script>
 
